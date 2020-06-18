a38fca1582dd94bb2498bd8d11f6631cb9aea1b4
Ryan Wattrus <09wattry@gmail.com>
2020-06-18 09:20:25 -0600

    Adding docs

---
+++ docs/precache-manifest.ddcf7cd60225bf18dc87726f692751f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426aeee1e98fbf28a15bedda0efa6f12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69e374547dfcaf56312",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97c1821ff8730e6e4209",
+    "url": "/static/js/2.d309ff22.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d309ff22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69e374547dfcaf56312",
+    "url": "/static/js/main.983fad51.chunk.js"
+  },
+  {
+    "revision": "1edc83aa3dddc281ae76",
+    "url": "/static/js/runtime-main.7b25127d.js"
+  }
+]);
