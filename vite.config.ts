a4488a4aa27c2b55b6c13e90863d93efcc5c0c0a
wattry <09wattry@gmail.com>
2026-06-12 06:57:55 -0700

    Fix CICD pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.PROD ? '/' : '/wattry.dev/',
   plugins: [react()],
   root: 'src',
   publicDir: resolve('public'),
