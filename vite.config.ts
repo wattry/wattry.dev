172938f23e07f69924ea973373777b0e524aa2cc
wattry <09wattry@gmail.com>
2026-06-13 11:45:34 -0700

    fix: update domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.PROD ? '/' : '/wattry.dev/',
+  // base: process.env.PROD ? '/' : '/wattry.dev/',
   plugins: [react()],
   root: 'src',
   publicDir: resolve('public'),
